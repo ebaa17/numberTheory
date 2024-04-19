36bc31ba23a75658dcba6380207e2267ded12931
mariemomar <113386463+mariemomar@users.noreply.github.com>
2024-04-19 05:54:56 +0200

    Update adminORuserNovels.js

--- javascript/adminORuserNovels.js
+++ javascript/adminORuserNovels.js
@@ -1,19 +1,29 @@
 window.onload = function(){
-    const admin_best_sales = document.querySelectorAll('.admin_best_sales');
-    const borrow_book = document.querySelectorAll('.borrow_book');
+    const borrowBtn = document.querySelectorAll('.borrow');
+    const editBtn = document.querySelectorAll('.edit');
+    const deleteBtn = document.querySelectorAll('.delete');
+    const addToCartBtn = document.querySelectorAll('.add-to-cart');
     const checkAdminforNovels = localStorage.getItem('checkAdmin');
-    console.log(admin_best_sales);
-    console.log(borrow_book);
+    // console.log(borrowBtn);
+    // console.log(editBtn);
+    // console.log(deleteBtn);
+    // console.log(addToCartBtn);
     console.log(checkAdminforNovels);
-    if(checkAdminforNovels === "true"){
+    if(checkAdminforNovels === "true"){ 
         console.log("its true")
-        admin_best_sales.forEach(element => { // Iterate over each element
+        editBtn.forEach(element => { 
+            element.classList.add("active");
+        });
+        deleteBtn.forEach(element => { 
             element.classList.add("active");
         });
     }
     else if(checkAdminforNovels === "false"){
         console.log("its false")
-        borrow_book.forEach(element => { // Iterate over each element
+        borrowBtn.forEach(element => { 
+            element.classList.add("active");
+        });
+        addToCartBtn.forEach(element => { 
             element.classList.add("active");
         });
     }
