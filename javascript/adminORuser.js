4565d2e151b14ee8b4a8261b38d1a252e450d45a
mariemomar <113386463+mariemomar@users.noreply.github.com>
2024-04-21 21:57:47 +0200

    Update adminORuser.js

--- javascript/adminORuser.js
+++ javascript/adminORuser.js
@@ -2,14 +2,25 @@ const signupinform = document.getElementById("signupinform");
 const checkboxAdmin = document.getElementById("checkboxAdmin");
 
 signupinform.addEventListener('click' , function(){
-    if(checkboxAdmin.checked){
+    const usernamevalue = document.getElementById("usernameInput").value;
+    console.log (usernamevalue);
+
+    if(checkboxAdmin.checked && usernamevalue !== null){
         localStorage.setItem('checkAdmin' , true);
         const admin = localStorage.getItem('checkAdmin');
         console.log(admin);
+        localStorage.setItem('userName' , usernamevalue);
+        const uname  = localStorage.getItem('userName');
+        console.log(uname);
+        window.location.href='accountUser.html';
     }
-    else{
+    else if (!checkboxAdmin.checked && usernamevalue !== null){
         localStorage.setItem('checkAdmin' , false);
         const admin = localStorage.getItem('checkAdmin');
         console.log(admin);
+        localStorage.setItem('userName' , usernamevalue);
+        const uname  = localStorage.getItem('userName');
+        console.log(uname);
+        window.location.href='accountUser.html';
     }
 });
