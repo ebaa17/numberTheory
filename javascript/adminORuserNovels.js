abf2dea8fd6ddec833a06a8c609ba2a11effc83b
mariemomar <113386463+mariemomar@users.noreply.github.com>
2024-04-21 15:44:43 +0200

    Update adminORuserNovels.js

--- javascript/adminORuserNovels.js
+++ javascript/adminORuserNovels.js
@@ -1,30 +1,12 @@
-window.onload = function(){
-    const borrowBtn = document.querySelectorAll('.borrow');
-    const editBtn = document.querySelectorAll('.edit');
-    const deleteBtn = document.querySelectorAll('.delete');
-    const addToCartBtn = document.querySelectorAll('.add-to-cart');
-    const checkAdminforNovels = localStorage.getItem('checkAdmin');
-    // console.log(borrowBtn);
-    // console.log(editBtn);
-    // console.log(deleteBtn);
-    // console.log(addToCartBtn);
-    console.log(checkAdminforNovels);
-    if(checkAdminforNovels === "true"){ 
-        console.log("its true")
-        editBtn.forEach(element => { 
-            element.classList.add("active");
-        });
-        deleteBtn.forEach(element => { 
-            element.classList.add("active");
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const isAdmin = localStorage.getItem('checkAdmin');
+    if (isAdmin === 'true') {
+        // Display div for admin
+        document.getElementById('admin').style.display = 'block';
+        document.getElementById('user').style.display = 'none';
+    } else {
+        // Display div for user
+        document.getElementById('admin').style.display = 'none';
+        document.getElementById('user').style.display = 'block';
     }
-    else if(checkAdminforNovels === "false"){
-        console.log("its false")
-        borrowBtn.forEach(element => { 
-            element.classList.add("active");
-        });
-        addToCartBtn.forEach(element => { 
-            element.classList.add("active");
-        });
-    }
-}
+});
