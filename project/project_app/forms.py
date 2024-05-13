66657d88e48daf1eaa8cee5d9b373be6d717184a
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-05-13 14:16:39 +0300

    Create forms.py

---
+++ project/project_app/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Book, Category
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(),
+        }
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+      
+        widgets = {
+            'category': forms.Select(),
+            'status': forms.Select(),
+            'price': forms.NumberInput(),
+            'id': forms.NumberInput(),
+            'retal_price_day': forms.NumberInput(),
+            'img': forms.FileInput(),
+            # 'img': forms.FilePathField(),
+        }
