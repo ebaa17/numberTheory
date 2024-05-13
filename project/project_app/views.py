6159f4112f2500c513942b1160227d4b7e52928b
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-05-13 14:21:10 +0300

    Update views.py

--- project/project_app/views.py
+++ project/project_app/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from .models import *
+from .forms import BookForm, CategoryForm
 
 # Create your views here.
+
+def view(request):
+    context = {
+        'category': Category.objects.all(),
+        'books': Book.objects.all(),
+    }
+    return render(request, 'pages/views.html', context)
+
+def addBook(request):
+    if request.method == 'POST':
+        add_book = BookForm(request.POST, request.FILES) # request.FILES  to save images
+        if add_book.is_valid():
+            add_book.save()
+
+        add_category = CategoryForm(request.POST)
+        if add_category.is_valid():
+            add_book.save()
+        
+        
+    context = {
+        'form': BookForm(),
+        'formCategory': CategoryForm(),
+    }
+    return render(request, 'pages/add_book.html', context)
+
+def cart(request):
+    return render(request, 'pages/cart.html')
+
+def main(request):
+    return render(request, 'pages/main.html')
+
+def details(request, id):
+    bookID = Book.objects.get(id=id)
+    context = {
+        'book': bookID
+    }
+    return render(request, 'pages/details.html', context)
