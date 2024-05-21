a69a0e40ed60d4590b8a150b14a815c2f68658c6
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-05-22 00:32:41 +0300

    Update urls.py

--- project/project_app/urls.py
+++ project/project_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('details/<str:id>/', views.details, name='details'),
     path('add_book/', views.add_book, name='add_book'),
     path('account/', views.account, name='account'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
