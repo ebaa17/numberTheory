46d5374cedfe60d224b4746c9e74b2dcfe2c0859
YassenAli <144853147+YassenAli@users.noreply.github.com>
2024-05-22 00:32:02 +0300

    Update models.py

--- project/project_app/models.py
+++ project/project_app/models.py
@@ -11,17 +11,16 @@ class Book(models.Model):
     status_book = [
         ('available', 'available'),
         ('borrowed', 'borrowed'),
-        ('sold', 'sold')
     ]
     name = models.CharField(max_length=100, verbose_name='Book Name')
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Price')
-    descritption = models.TextField(verbose_name='Description')
+    description = models.TextField(verbose_name='Description')
     id = models.IntegerField(verbose_name='Book ID', primary_key=True)
     author = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     img = models.ImageField(upload_to='photos/%y/%m/%d', null=True, blank=True)
-    available = models.BooleanField(default=True)
-    status = models.CharField(max_length=50, choices=status_book)
+    # available = models.BooleanField(default=True)
+    status = models.CharField(max_length=50, choices=status_book, default='available', verbose_name='Status')
 
     def __str__(self):
         return self.name
